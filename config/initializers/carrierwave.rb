a8a51ab2f4020067cfa0394178f49f4469290f2d
Brian Durbin <kits119710@gmail.com>
2015-11-27 00:48:32 -0700

    carrierwave dropbox implementation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,6 @@ CarrierWave.configure do |config|
   config.dropbox_app_secret = ENV["s8l9qs0v5aa5wpe"]
   config.dropbox_access_token = ENV["d6917l5mll7uz6nw"]
   config.dropbox_access_token_secret = ENV["tc2kh6408xt2fua"]
-  config.dropbox_user_id = ENV["USER_ID"]
-  config.dropbox_access_type = "383273960"
+  config.dropbox_user_id = ENV["383273960"]
+  config.dropbox_access_type = "dropbox"
 end
