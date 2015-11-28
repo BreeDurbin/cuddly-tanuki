18abe4750e24f98aa6eb814d5ffe08f3e9fa9f15
Brian Durbin <kits119710@gmail.com>
2015-11-28 04:49:18 -0700

    Added devise authentication functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:home, :show]
 
   def home
     @posts=Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
