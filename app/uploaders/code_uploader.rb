ab27dd698bfeabf818a7f87c186f832c1469a8f8
Brian Durbin <kits119710@gmail.com>
2015-11-28 02:01:47 -0700

    CARRIERWAVEwith dropbox NEEDS TO WORK DAMNIT

--- app/uploaders/code_uploader.rb
+++ app/uploaders/code_uploader.rb
@@ -14,7 +14,7 @@ class CodeUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/{model.id}
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.dropbox_app_key = "x5aiqifcmtsmk33"
-  config.dropbox_app_secret = "s8l9qs0v5aa5wpe"
-  config.dropbox_access_token = "d6917l5mll7uz6nw"
-  config.dropbox_access_token_secret = "tc2kh6408xt2fua"
+  config.dropbox_app_key = "87fv0dx0flz3uox"
+  config.dropbox_app_secret = "jbkhjspk6quu2ly"
+  config.dropbox_access_token = "0qxglr0rissz919u"
+  config.dropbox_access_token_secret = "ix3nimghs18diw3"
   config.dropbox_user_id = "383273960"
   config.dropbox_access_type = "dropbox"
 end
