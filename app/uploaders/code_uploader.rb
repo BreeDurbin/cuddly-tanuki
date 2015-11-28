49d564a847dfcf010e1dcadc1d5a6a138dc03cfa
Brian Durbin <kits119710@gmail.com>
2015-11-28 01:08:53 -0700

    CARRIERWAVEwith dropbox NEEDS TO WORK DAMNIT

--- app/uploaders/code_uploader.rb
+++ app/uploaders/code_uploader.rb
@@ -14,7 +14,7 @@ class CodeUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
 
   def store_dir
-    /#{model.class.to_s.underscore}/{mounted_as}/
+    /{model.class.to_s.underscore}/{mounted_as}/
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
