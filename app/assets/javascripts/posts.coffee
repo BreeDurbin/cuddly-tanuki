e50896f502d4378d0eb6dbe69c993fad8c124e69
Brian Durbin <kits119710@gmail.com>
2015-12-01 01:35:18 -0700

    PLEASE WORK JAVASCRIPT!

--- app/assets/javascripts/posts.coffee
+++ app/assets/javascripts/posts.coffee
@@ -3,8 +3,6 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 
-alert("javascript up");
-
 
 
 
--- public/assets/application-e86f251bc94c49e6afee241eb0f518cb7095e88f0544111172c0779b5013993d.js
+++ public/assets/application-e86f251bc94c49e6afee241eb0f518cb7095e88f0544111172c0779b5013993d.js
@@ -11649,15 +11649,11 @@ return jQuery;
 
 }).call(this);
 (function() {
-
-
-}).call(this);
-(function() {
-
-
-}).call(this);
-(function() {
-
+  $('.js-accordion-trigger').bind('click', function(e) {
+    jQuery(this).parent().find('.submenu').slideToggle('fast');
+    jQuery(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
 
 }).call(this);
 (function() {
@@ -11685,13 +11681,7 @@ return jQuery;
 //REFILLS------------------------------------------------------------------------------------
 
 
-//=============================ACCORDION===============================================
 
-$('.js-accordion-trigger').bind('click', function(e){
-  jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
-  jQuery(this).parent().toggleClass('is-expanded');
-  e.preventDefault();
-});
 
 //==========================++NAVBAR JS++===============================================
 
@@ -11710,8 +11700,8 @@ $(document).ready(function() {
 });
 
 
+
+
 //======================================================================================
 
-//test
-
-alert("hello");
+//alert("javascript up");
