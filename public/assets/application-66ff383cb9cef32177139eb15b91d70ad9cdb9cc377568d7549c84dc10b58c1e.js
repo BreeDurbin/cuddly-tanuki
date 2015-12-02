67e14e759fbebad530e50aa905dd9886817d5616
Brian Durbin <kits119710@gmail.com>
2015-12-02 00:12:12 -0700

    PLEASE WORK JAVASCRIPT!

--- public/assets/application-66ff383cb9cef32177139eb15b91d70ad9cdb9cc377568d7549c84dc10b58c1e.js
+++ public/assets/application-66ff383cb9cef32177139eb15b91d70ad9cdb9cc377568d7549c84dc10b58c1e.js
@@ -11649,13 +11649,7 @@ return jQuery;
 
 }).call(this);
 (function() {
-  alert("javascript up");
 
-  $('.js-accordion-trigger').bind('click', function(e) {
-    jQuery(this).parent().find('.submenu').slideToggle('fast');
-    jQuery(this).parent().toggleClass('is-expanded');
-    e.preventDefault();
-  });
 
 }).call(this);
 (function() {
@@ -11707,3 +11701,9 @@ $(document).ready(function() {
 //======================================================================================
 
 alert("javascript up");
+//=======================================================================================
+$('.js-accordion-trigger').bind('click', function(e){
+  jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+  jQuery(this).parent().toggleClass('is-expanded');
+  e.preventDefault();
+});
