add00989829f3b5fbc757ba7dbdb7c45115c50b2
Brian Durbin <kits119710@gmail.com>
2015-12-27 16:39:05 -0700

    PLEASE WORK JAVASCRIPT!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
 
-  def aboutMe
-  end
-  
+	
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,24 +1,7 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get aboutMe" do
-    get :aboutMe
-    assert_response :success
-  end
-
-  test "should get fileArchive" do
-    get :fileArchive
-    assert_response :success
-  end
-
-  test "should get home" do
-    get :home
-    assert_response :success
-  end
-
-  test "should get projects" do
-    get :projects
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
